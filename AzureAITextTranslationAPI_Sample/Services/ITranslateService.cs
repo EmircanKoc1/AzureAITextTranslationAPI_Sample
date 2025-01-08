d9ba0f6301c16e59b8a470dcaab0cc6643d6160a
Emircan <emircandv787@gmail.com>
2025-01-08 23:58:27 +0300

    The ITranslateService interface and TranslateService class have been defined.

---
+++ AzureAITextTranslationAPI_Sample/Services/ITranslateService.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureAITextTranslationAPI_Sample.Services
+{
+    public interface ITranslateService
+    {
+        Task<string> TranslateAsync(string sourceLanguage, string targetLanguage, string text);
+
+        
+    }
+}
---
+++ AzureAITextTranslationAPI_Sample/Services/TranslateService.cs
@@ -0,0 +1,25 @@
+﻿
+using Azure.AI.Translation.Text;
+
+namespace AzureAITextTranslationAPI_Sample.Services
+{
+    public class TranslateService : ITranslateService
+    {
+        private readonly TextTranslationClient _textTranslationClient;
+
+        public TranslateService(TextTranslationClient textTranslationClient)
+        {
+            _ = textTranslationClient ?? throw new ArgumentNullException("The TextTranslationClient instance can not be null");
+            _textTranslationClient = textTranslationClient;
+        }
+
+        public async Task<string?> TranslateAsync(string sourceLanguage, string targetLanguage, string text)
+        {
+            var translatedResponse = await _textTranslationClient.TranslateAsync(targetLanguage, text, sourceLanguage);
+
+            //return translateResponse.Value.FirstOrDefault().Translations;
+
+            return translatedResponse.Value.FirstOrDefault()?.Translations.FirstOrDefault()?.Text;
+        }
+    }
+}
