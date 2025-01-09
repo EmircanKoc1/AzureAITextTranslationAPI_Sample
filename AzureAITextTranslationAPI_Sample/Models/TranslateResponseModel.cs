a6250efde759e151fc809effe1db1af76b28666c
Emircan <emircandv787@gmail.com>
2025-01-09 23:35:53 +0300

    The AzureAITranslateOption class has been added. The AzureAITranslateOption json has been added to the appsettings.json. The TranslateResponseModel class has been added.

---
+++ AzureAITextTranslationAPI_Sample/Models/TranslateResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureAITextTranslationAPI_Sample.Models;
+
+public record TranslateResponseModel(
+    string sourceLang,
+    string targetLang,
+    string content,
+    string? translatedContent,
+    string? errorMessage,
+    bool isSuccess);
+
---
+++ AzureAITextTranslationAPI_Sample/Options/AzureAITranslateOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureAITextTranslationAPI_Sample.Options
+{
+    public class AzureAITranslateOptions
+    {
+        public string Azure { get; set; } = nameof(AzureAITranslateOptions);
+
+        public string APIKey { get; set; }
+        public string Region { get; set; }
+        public string Endpoint { get; set; }
+
+
+
+    }
+}
