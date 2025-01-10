827d43b921d4da212e694b0c97f2556e342b676f
Emircan <emircandv787@gmail.com>
2025-01-10 23:21:03 +0300

    The ITranslateService interface and TranslateService class have been completed.

--- AzureAITextTranslationAPI_Sample/Services/ITranslateService.cs
+++ AzureAITextTranslationAPI_Sample/Services/ITranslateService.cs
@@ -1,9 +1,15 @@
-﻿namespace AzureAITextTranslationAPI_Sample.Services
+﻿using AzureAITextTranslationAPI_Sample.Models;
+
+namespace AzureAITextTranslationAPI_Sample.Services
 {
     public interface ITranslateService
     {
-        Task<string> TranslateAsync(string sourceLanguage, string targetLanguage, string text);
+        Task<TranslateResponseModel> TranslateAsync(string sourceLanguage, string targetLanguage, string content);
+        Task<IEnumerable<TranslateResponseModel>> TranslateAsync(string sourceLanguage, IEnumerable<string> targetLanguages, string content);
 
-        
+        Task<IEnumerable<TranslateResponseModel>> TranslateAsync(string sourceLanguage, IEnumerable<string> targetLanguages, IEnumerable<string> contents);
+
+        //Task<IEnumerable<TranslateResponseModel>> TranslateAsync(string sourceLanguage, string targetLanguage, IEnumerable<string> contents);
+
     }
 }
--- AzureAITextTranslationAPI_Sample/Services/TranslateService.cs
+++ AzureAITextTranslationAPI_Sample/Services/TranslateService.cs
@@ -1,25 +1,156 @@
 ﻿
+using Azure;
 using Azure.AI.Translation.Text;
+using AzureAITextTranslationAPI_Sample.Models;
+using System.Net;
 
 namespace AzureAITextTranslationAPI_Sample.Services
 {
     public class TranslateService : ITranslateService
     {
         private readonly TextTranslationClient _textTranslationClient;
+        private readonly ILogger<TranslateService> _logger;
 
-        public TranslateService(TextTranslationClient textTranslationClient)
+        public TranslateService(TextTranslationClient textTranslationClient, ILogger<TranslateService> logger)
         {
             _ = textTranslationClient ?? throw new ArgumentNullException("The TextTranslationClient instance can not be null");
             _textTranslationClient = textTranslationClient;
+            _logger = logger;
         }
 
-        public async Task<string?> TranslateAsync(string sourceLanguage, string targetLanguage, string text)
+        public async Task<TranslateResponseModel> TranslateAsync(string sourceLanguage, string targetLanguage, string content)
         {
-            var translatedResponse = await _textTranslationClient.TranslateAsync(targetLanguage, text, sourceLanguage);
 
-            //return translateResponse.Value.FirstOrDefault().Translations;
+            var translateOptions = new TextTranslationTranslateOptions(
+             targetLanguages: [targetLanguage],
+             sourceLanguage: sourceLanguage,
+             content: [content]);
 
-            return translatedResponse.Value.FirstOrDefault()?.Translations.FirstOrDefault()?.Text;
+
+            var responseObject = new TranslateResponseModel(
+                sourceLang: sourceLanguage,
+                targetLang: targetLanguage,
+                content: content,
+                translatedContent: null,
+                errorMessage: null,
+                isSuccess: false);
+
+            try
+            {
+
+                var result = await _textTranslationClient.TranslateAsync(translateOptions);
+                var translationText = result.Value?.FirstOrDefault()?.Translations.FirstOrDefault();
+
+                return responseObject with
+                {
+                    translatedContent = translationText?.Text,
+                    isSuccess = true
+                };
+            }
+            catch (RequestFailedException ex) when (ex.Status is (int)HttpStatusCode.BadRequest)
+            {
+                _logger.LogInformation(ex.Message);
+                return responseObject with
+                {
+                    translatedContent = null,
+                    errorMessage = ex.Message
+                };
+            }
+            catch (RequestFailedException ex)
+            {
+                _logger.LogInformation(ex.Message);
+                throw;
+            }
+            catch (ArgumentNullException ex)
+            {
+                _logger.LogInformation(ex.Message);
+                throw;
+            }
+
+
         }
+
+        public async Task<IEnumerable<TranslateResponseModel>?> TranslateAsync(string sourceLanguage, IEnumerable<string> targetLanguages, string content)
+        {
+            try
+            {
+                var textTranslationOptions = new TextTranslationTranslateOptions(
+              targetLanguages: targetLanguages,
+              sourceLanguage: sourceLanguage,
+              content: [content]);
+                var translateResponse = await _textTranslationClient.TranslateAsync(textTranslationOptions);
+                return translateResponse?.Value?.FirstOrDefault()?.Translations?.Select(tt =>
+                  {
+                      return new TranslateResponseModel(
+                          sourceLang: sourceLanguage,
+                          targetLang: tt.TargetLanguage,
+                          content: content,
+                          translatedContent: tt.Text,
+                          errorMessage: null,
+                          isSuccess: true);
+                  });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.Message);
+                throw;
+            }
+
+
+
+
+        }
+
+        public async Task<IEnumerable<TranslateResponseModel>?> TranslateAsync(string sourceLanguage, IEnumerable<string> targetLanguages, IEnumerable<string> contents)
+        {
+            try
+            {
+
+                var translateOptions = new TextTranslationTranslateOptions(
+                   targetLanguages: targetLanguages,
+                   content: contents,
+                   sourceLanguage: sourceLanguage);
+
+                var translateResponse = await _textTranslationClient.TranslateAsync(translateOptions);
+
+
+                var responseList = new List<TranslateResponseModel>();
+
+
+
+                foreach (var tti in translateResponse.Value)
+                {
+
+
+                    foreach (var tt in tti.Translations)
+                    {
+                        responseList.Add(new TranslateResponseModel(
+                           sourceLang: sourceLanguage,
+                           targetLang: tt.TargetLanguage,
+                           content: null,
+                           translatedContent: tt.Text,
+                           errorMessage: null,
+                           isSuccess: true));
+
+
+                    }
+
+                }
+
+
+
+                return responseList;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.Message);
+                throw;
+            }
+
+
+
+        }
+
+
     }
 }
