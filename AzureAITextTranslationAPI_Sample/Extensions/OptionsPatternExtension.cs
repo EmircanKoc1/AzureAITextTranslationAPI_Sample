c2dc31bb77249b6c2f8b46dc27779f24b983e49f
Emircan <emircandv787@gmail.com>
2025-01-10 23:20:32 +0300

    The Options pattern class has been created and defined in the appsettings.json file.

---
+++ AzureAITextTranslationAPI_Sample/Extensions/OptionsPatternExtension.cs
@@ -0,0 +1,46 @@
+﻿using Azure.AI.Translation.Text;
+using Azure;
+using AzureAITextTranslationAPI_Sample.Options;
+using Microsoft.Extensions.Options;
+
+namespace AzureAITextTranslationAPI_Sample.Extensions
+{
+    public static class OptionsPatternExtension
+    {
+        public static IServiceCollection AddAzureAITranslateOptions(this IServiceCollection services)
+        {
+
+            services
+                .AddOptions<AzureAITranslateOptions>()
+                .BindConfiguration(AzureAITranslateOptions.Azure)
+                .Validate(option =>
+                {
+                    if (option.APIKey is null) return false;
+
+                    if (option.Endpoint is null) return false;
+
+                    if (option.Region is null) return false;
+
+                    return true;
+                })
+                .ValidateOnStart();
+
+            services.AddSingleton<TextTranslationClient>(provider =>
+            {
+                var service = provider.GetRequiredService<IOptions<AzureAITranslateOptions>>();
+
+                var options = service.Value;
+
+                string apiKey = options.APIKey;
+                string region = options.Region;
+                string endpoint = options.Endpoint;
+
+                return new TextTranslationClient(new AzureKeyCredential(apiKey), new Uri(endpoint), region);
+            });
+
+            return services;
+        }
+
+
+    }
+}
--- AzureAITextTranslationAPI_Sample/Options/AzureAITranslateOptions.cs
+++ AzureAITextTranslationAPI_Sample/Options/AzureAITranslateOptions.cs
@@ -2,7 +2,7 @@
 {
     public class AzureAITranslateOptions
     {
-        public string Azure { get; set; } = nameof(AzureAITranslateOptions);
+        public const string Azure = "AzureAITranslateOptions";
 
         public string APIKey { get; set; }
         public string Region { get; set; }
