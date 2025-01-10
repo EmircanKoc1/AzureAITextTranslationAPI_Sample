e94d5d67b106098f2c0e5bc8aee3188fc1fbc86c
Emircan <emircandv787@gmail.com>
2025-01-10 23:19:06 +0300

    The Request and response models have been created.

---
+++ AzureAITextTranslationAPI_Sample/Models/TranslateRequestModels.cs
@@ -0,0 +1,6 @@
+﻿namespace AzureAITextTranslationAPI_Sample.Models;
+
+public record TranslateRequestModel(string sourceLang, string targetLang, string content);
+public record TranslateMultiTargetLangRequestModel(string sourceLang, IEnumerable<string> targetLangs, string content);
+
+public record TranslateMultiContentAndTargetLangRequestModel(string sourceLang, IEnumerable<string> targetLangs, IEnumerable<string> contents);
--- AzureAITextTranslationAPI_Sample/Models/TranslateResponseModels.cs
+++ AzureAITextTranslationAPI_Sample/Models/TranslateResponseModels.cs
@@ -8,3 +8,5 @@ public record TranslateResponseModel(
     string? errorMessage,
     bool isSuccess);
 
+
+
